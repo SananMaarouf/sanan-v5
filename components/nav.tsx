67ec7e141e5e88ae043fb40904e67a86db057d15
Sanan Maarouf <sanan.adnan97@gmail.com>
2024-12-02 14:22:17 +0100

    underline nav element

--- components/nav.tsx
+++ components/nav.tsx
@@ -68,9 +68,13 @@ export default function Navbar() {
       <div className="flex justify-end w-full md:hidden">
         <button onClick={toggleMenu} className="focus:outline-none z-50">
           {isOpen ? (
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor" className='w-10 h-10 text-orange-600'><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor" className='w-10 h-10 text-white hover:text-orange-600'>
+              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+            </svg>
           ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor" className='w-10 h-10 text-orange-600'><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor" className='w-10 h-10 text-white hover:text-orange-600'>
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
           )}        </button>
         <AnimatePresence>
           {isOpen && (
@@ -107,7 +111,7 @@ export default function Navbar() {
 
                   <motion.div variants={itemVariants}>
                     <Dialog>
-                      <DialogTrigger className='font-silkscreen hover:text-orange-600'>{t("nav.contact")}</DialogTrigger>
+                      <DialogTrigger className='font-silkscreen hover:text-orange-600 hover:underline underline-offset-2'>{t("nav.contact")}</DialogTrigger>
                       <DialogContent className='font-silkscreen'>
                         <DialogHeader>
                           <DialogTitle>{t("nav.platforms")} </DialogTitle>

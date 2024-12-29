d9f1754a80bb24b73820aacf395de1fc36544231
Sanan Maarouf <sanan.adnan97@gmail.com>
2024-12-29 22:33:15 +0100

    Refactored projectsCarousel + deleted projects page

--- components/projectsCarousel.tsx
+++ components/projectsCarousel.tsx
@@ -1,92 +1,148 @@
 import React, { useRef } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import Link from "next/link";
-import Image from "next/image";
 import { useTranslation } from "next-i18next";
 
-const arrowMotion = {
-  rest: {
-    x: 0,
-    transition: { duration: 0.1 },
-  },
-  hover: {
-    x: 10,
-    transition: { duration: 0.2 },
-  },
-};
-
-const textMotion = {
-  rest: {
-    opacity: 1,
-    transition: { duration: 0.1 },
-  },
-  hover: {
-    scale: 1.1,
-    transition: { duration: 0.1 },
-    tap: { scale: 0.9 },
-  },
-};
-
 export default function ProjectsCarousel() {
-  const ref = useRef<HTMLDivElement>(null);
-  const { t } = useTranslation();
-  const projects = [
-    { url: "/projects/dubistro", src: "/projects/dubistro.png", alt: "Dubistro", title: "O1", description: "Dubistro" },
-    { url: "/projects/applitrack", src: "/projects/applitrack.webp", alt: "Applitrack", title: "O2", description: "Applitrack" },
-    { url: "/projects/partnersos", src: "/projects/psos.webp", alt: "Partner SOS", title: "O3", description: "Partner-SOS" },
-    { url: "/projects/sanplan", src: "/projects/sanplan.webp", alt: "SanPlan", title: "O4", description: "SanPlan" },
-    { url: "/projects/sara", src: "/projects/zamec.webp", alt: "Sara", title: "O5", description: "Sara" },
-    { url: "/projects/insultifier", src: "/projects/insultifier.webp", alt: "Insultifier", title: "O6", description: "Insultifier" },
-    { url: "/projects/greentalk", src: "/projects/greentalk.webp", alt: "Greentalk", title: "O7", description: "Greentalk" },
-  ];
+	const ref = useRef<HTMLDivElement>(null);
+	const { t } = useTranslation();
+	const projects = [
+		{
+			number: "O1",
+			alt: "Dubistro",
+			title: "Dubistro",
+			description: t('projectsDescriptions.dubistro'),
+			url: "/projects/dubistro",
+			src: "/projects/dubistro.png",
+			type: t('projectsTypes.web'),
+			tech: ["React", "Next.js", "TailwindCSS", "Sanity-CMS"],
+		},
+		{
+			number: "O2",
+			alt: "Applitrack",
+			title: "Applitrack",
+			description: t('projectsDescriptions.applitrack'),
+			url: "/projects/applitrack",
+			src: "/projects/applitrack.webp",
+			type: t('projectsTypes.web'),
+			tech: ["React", "Next.js", "TailwindCSS", "PocketBase"],
+		},
+		{
+			number: "O3",
+			alt: "Partner SOS",
+			title: "Partner-SOS",
+			description: t('projectsDescriptions.partnersos'),
+			url: "/projects/partnersos",
+			src: "/projects/psos.webp",
+			type: t('projectsTypes.both'),
+			tech: ["React Native", "React", "Next.js", "TailwindCSS", "Firebase"],
+		},
+		{
+			number: "O4",
+			alt: "SanPlan",
+			title: "SanPlan",
+			description: t('projectsDescriptions.sanplan'),
+			url: "/projects/sanplan",
+			src: "/projects/sanplan.webp",
+			type: t('projectsTypes.mobile'),
+			tech: ["React Native", "Expo Go", "SQLite",],
+		},
+		{
+			number: "O5",
+			alt: "Sarazamecznik.com",
+			title: "Zamec",
+			description: t('projectsDescriptions.sara'),
+			url: "/projects/sara",
+			src: "/projects/zamec.webp",
+			type: t('projectsTypes.web'),
+			tech: ["React", "Next.js", "TailwindCSS", "Contentful-CMS"],
+		},
+		{
+			number: "O6",
+			alt: "Greentalk",
+			title: "Greentalk",
+			description: t('projectsDescriptions.greentalk'),
+			url: "/projects/greentalk",
+			src: "/projects/greentalk.webp",
+			type: t('projectsTypes.web'),
+			tech: ["PHP", "HTML", "CSS", "JS", "MySQL"],
+		},
+		{
+			number: "O7",
+			alt: "Insultifier",
+			title: "Insultifier",
+			description: t('projectsDescriptions.insultifier'),
+			url: "/projects/insultifier",
+			src: "/projects/insultifier.webp",
+			type: t('projectsTypes.joke'),
+			tech: ["React", "Next.js", "TailwindCSS"],
+		},
+	];
 
-  return (
-    <AnimatePresence>
-      <motion.section
-        ref={ref}
-        initial={{ opacity: 0, y: 200 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{delay:0.6, duration: 1 }}
-        className="
-        my-10 
-        md:mx-auto font-silkscreen 
-        relative w-full xl:w-10/12
-        "
-      >
-        <h1 className="text-3xl mb-2">{t("projects.title")}</h1>
-        <motion.p animate={{ scale: [1, 1.1, 1] }} transition={{ repeat: 10, duration: 2 }} className="block xl:hidden absolute right-2 top-2">
-          swipe ➡️
-        </motion.p>
-        <motion.p animate={{ scale: [1, 1.1, 1] }} transition={{ repeat: 10, duration: 2 }} className="hidden xl:block absolute right-2 top-2">
-          scroll ➡️
-        </motion.p>
-        {/* Simple horizontal slider */}
-        <div className="
-          overflow-x-scroll flex 
-          gap-6 py-4
-          ">
-          {projects.map((project, index) => (
-            <motion.div
-              key={index}
-              className="shrink-0 w-72 h-80 bg-white rounded-xl transition duration-300 ease-linear hover:bg-deco text-deco"
-            >
-              <Link
-                href={project.url}
-                className="hover:text-white w-full h-full flex flex-col"
-              >
-                <div className="p-4 flex-grow">
-                  <h2 className="text-2xl">{project.title}</h2>
-                  <p className="text-2xl">{project.description}</p>
-                  <Image src={project.src} alt={project.alt} width={300} height={300} className="rounded-xl object-cover h-44"/>               
-                </div>
-                <div className="py-2 pl-4 hover:underline decoration-2">
-                  {t("projects.viewProject")}
-                </div>
-              </Link>
-            </motion.div>
-          ))}
-        </div>
-      </motion.section>
-    </AnimatePresence>
-  );
+	return (
+		<AnimatePresence>
+			<motion.section
+				ref={ref}
+				initial={{ opacity: 0, y: 200 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ delay: 0.6, duration: 1 }}
+				className="my-10 md:mx-auto font-silkscreen relative w-full xl:w-10/12"
+			>
+				{/* indicate to user */}
+				<h1 className="text-3xl mb-2">{t("projects.title")}</h1>
+				<motion.p
+					animate={{ scale: [1, 1.1, 1] }}
+					transition={{ repeat: 10, duration: 2 }}
+					className="block xl:hidden absolute right-2 top-2"
+				>
+					swipe ➡️
+				</motion.p>
+				<motion.p
+					animate={{ scale: [1, 1.1, 1] }}
+					transition={{ repeat: 10, duration: 2 }}
+					className="hidden xl:block absolute right-2 top-2"
+				>
+					scroll ➡️
+				</motion.p>
+
+				{/* horizontal slider */}
+				<div className="overflow-x-scroll flex gap-6 py-4">
+					{projects.map((project, index) => (
+						<motion.div key={index}
+							className="group shrink-0 w-72 h-64 bg-white rounded-xl transition duration-300 ease-linear hover:bg-deco 
+							text-deco">
+							<Link href={project.url} className="hover:text-white w-full h-full flex flex-col relative">
+								<div className="p-4 flex-grow">
+									{/* project number and type  */}
+									<div className="flex justify-between">
+										<h2 className="text-2xl">{project.number}</h2>
+										<p className="text-sm my-auto">{project.type}</p>
+									</div>
+
+									{/* project title */}
+									<p className="text-2xl">{project.title}</p>
+									
+									{/* project description */}
+									<div className="text-sm flex items-center h-20">
+										<p>{project.description}</p>
+									</div>
+								
+								</div>
+
+								{/* tech stack tags */}
+								<div className="flex flex-wrap-reverse gap-1 p-1 mr-1 mb-1 h-14 justify-end">
+									{project.tech.map((tech, i) => (
+										<span key={i} className="bg-deco text-white h-6 text-xs rounded-md p-1 group-hover:bg-white group-hover:text-deco">
+											{tech}
+										</span>
+									))}
+								</div>
+
+							</Link>
+						</motion.div>
+					))}
+				</div>
+			</motion.section>
+		</AnimatePresence>
+	);
 }

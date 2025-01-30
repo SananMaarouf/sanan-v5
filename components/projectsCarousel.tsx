17aaecfbc34c3e0d6335b26a06c7ae94639f3613
Sanan Maarouf <sanan.adnan97@gmail.com>
2025-01-30 21:27:11 +0100

    please linter

--- components/projectsCarousel.tsx
+++ components/projectsCarousel.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { motion, AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
 
